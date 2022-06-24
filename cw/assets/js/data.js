18a55b42d6d24fc10b49c1c685b9bc453da06edb
anonymhaus <76622609+anonymhaus@users.noreply.github.com>
2022-06-24 06:25:56 -0700

    :+1:

---
+++ cw/assets/js/data.js
@@ -0,0 +1,174 @@
+var DataCenter = {
+    drugs: [
+
+        {
+            "name": "aave",
+            "minimum_price": 28,
+            "maximum_price": 632,
+            "expensive": {
+                "minimum_price": 28,
+                "maximum_price": 632,
+            }
+        },
+
+        {
+            "name": "ada",
+            "minimum_price": 1,
+            "maximum_price": 2,
+            "expensive": {
+                "minimum_price": 1,
+                "maximum_price": 2,
+            }
+        },
+        {
+            "name": "alcx",
+            "minimum_price": 20,
+            "maximum_price": 2004,
+        },
+
+        {
+            "name": "bnb",
+            "minimum_price": 10,
+            "maximum_price": 700,
+            "cheap": {
+                "minimum_price": 10,
+                "maximum_price": 700,
+            }
+        },
+
+        {
+            "name": "btc",
+            "minimum_price": 5000,
+            "maximum_price": 69000,
+            "cheap": {
+                "minimum_price": 5000,
+                "maximum_price": 69000,
+            },
+            "expensive": {
+                "minimum_price": 5000,
+                "maximum_price": 69000,
+            }
+        },
+
+        {
+            "name": "doge",
+            "minimum_price": 69,
+            "maximum_price": 420,
+            "expensive": {
+                "minimum_price": 69,
+                "maximum_price": 420,
+            }
+        },
+
+        {
+            "name": "eth",
+            "minimum_price": 100,
+            "maximum_price": 5000,
+            "expensive": {
+                "minimum_price": 100,
+                "maximum_price": 5000,
+            }
+        },
+
+        {
+            "name": "dot",
+            "minimum_price": 1,
+            "maximum_price": 54,
+            "cheap": {
+                "minimum_price": 1,
+                "maximum_price": 54,
+            }
+        },
+
+        {
+            "name": "mkr",
+            "minimum_price": 206,
+            "maximum_price": 6012,
+            "cheap": {
+                "minimum_price": 206,
+                "maximum_price": 6012,
+            }
+        },
+
+        {
+            "name": "sol",
+            "minimum_price": 1,
+            "maximum_price": 260,
+        },
+
+        {
+            "name": "uni",
+            "minimum_price": 2,
+            "maximum_price": 43,
+        },
+
+
+    ],
+    cities: [
+        {
+            "name": "laval",
+            "cops": 8,
+            "min_drugs": 100,
+            "max_drugs": 101,
+        },
+        {
+            "name": "mont-royal",
+            "cops": 7,
+            "min_drugs": 13,
+            "max_drugs": 14,
+        },
+        {
+            "name": "lafontaine-park",
+            "cops": 6,
+            "min_drugs": 11,
+            "max_drugs": 12,
+        },
+        {
+            "name": "berry-uqam-metro",
+            "cops": 5,
+            "min_drugs": 9,
+            "max_drugs": 10,
+        },
+        {
+            "name": "sainte-helene-island",
+            "cops": 4,
+            "min_drugs": 7,
+            "max_drugs": 8,
+        },
+        {
+            "name": "vieux-port",
+            "cops": 3,
+            "min_drugs": 5,
+            "max_drugs": 6,
+        },
+        {
+            "name": "westmount",
+            "cops": 2,
+            "min_drugs": 3,
+            "max_drugs": 4,
+        },
+        {
+            "name": "notre-dame-de-grace",
+            "cops": 1,
+            "min_drugs": 1,
+            "max_drugs": 2,
+        },
+    ],
+    messages: [
+        100000000,
+        1000000,
+        400000,
+        300000,
+        200000,
+        100000,
+        50000,
+        10000,
+        5000,
+        1000,
+        0,
+        -4050,
+        -10000,
+        -99999,
+        -100000,
+    ]
+};
