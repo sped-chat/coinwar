78032ef3f7ec37e53e74ea41d64bd2382d6e7cf2
COINWAR.APP <15058713+thecoinwar@users.noreply.github.com>
2022-07-20 06:41:16 -0700

    :+1:

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: '/Applications/MAMP/htdocs/thecoinwar.github.io',
+	globPatterns: [
+		'**/*.{png,html,json}'
+	],
+	swDest: '/Applications/MAMP/htdocs/thecoinwar.github.io/sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
+};
